ef13e0105e2868bcef50f30a4685a3b595d33c2d
Dragoness11421 <nicholsonj2@carleton.edu>
2025-04-25 17:44:28 -0500

    Update processor.py

--- ProductionCode/processor.py
+++ ProductionCode/processor.py
@@ -6,6 +6,15 @@ data = [
             {'datetime': '10/10/1956 21:00', 'city': 'edna', 'shape': 'cylinder'}
         ]
 
+def display_results(results):
+    """Display search results."""
+    if not results:
+        print("No sightings found matching your query, please try again with different parameters.")
+        return    
+    for row in results:
+        print(row)
+
+
 def filter_by_shape(data, shape):
     """filter data to match the given shape."""
     results = []
