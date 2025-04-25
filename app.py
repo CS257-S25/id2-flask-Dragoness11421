9637a0e513bef43557924174e0db4a16e12d0533
Dragoness11421 <nicholsonj2@carleton.edu>
2025-04-25 18:13:54 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,21 @@
 '''Replace me with your flask app'''
+from ProductionCode.processor import (
+    display_results,
+    filter_by_shape,
+    get_sightings_by_shape
+)
+import cl
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('shape:/<shape>')
+  results = get_sightings_by_shape(shape)
+  return display_results(results)
+
+
+
+
+
+
+if __name__ == '__main__':
+  app.run(host='0.0.0.0', port=81)
