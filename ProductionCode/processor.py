15559ef8766a6db40f24ef20c3593c6c2ca9d212
Dragoness11421 <nicholsonj2@carleton.edu>
2025-04-25 18:22:18 -0500

    Update processor.py

--- ProductionCode/processor.py
+++ ProductionCode/processor.py
@@ -1,11 +1,5 @@
 """Helper functions for loading and filtering UFO sightings data."""
 
-data = [
-            {'datetime': '10/10/1949 20:30', 'city': 'san marcos', 'shape': 'cylinder'},
-            {'datetime': '10/10/1949 21:00', 'city': 'lackland afb', 'shape': 'light'},
-            {'datetime': '10/10/1956 21:00', 'city': 'edna', 'shape': 'cylinder'}
-        ]
-
 def display_results(results):
     """Display search results."""
     if not results:
@@ -27,5 +21,9 @@ def filter_by_shape(data, shape):
 
 def get_sightings_by_shape(shape):
     """Get sightings by given shape."""
-    data = load_data()
+    data = [
+            {'datetime': '10/10/1949 20:30', 'city': 'san marcos', 'shape': 'cylinder'},
+            {'datetime': '10/10/1949 21:00', 'city': 'lackland afb', 'shape': 'light'},
+            {'datetime': '10/10/1956 21:00', 'city': 'edna', 'shape': 'cylinder'}
+        ]
     return filter_by_shape(data, shape)
