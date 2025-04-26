b39f8ef148f728a4cc05281a1b08356986d3eb84
Dragoness11421 <nicholsonj2@carleton.edu>
2025-04-25 20:47:36 -0500

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,13 @@ import cl
 from flask import Flask
 app = Flask(__name__)
 
+@app.route('/')
+def home_screen()
+    print("type in shape:/<the shape of the ufos you want to look up> to look up those ufos")
+
+
 @app.route('shape:/<shape>')
+def display_ufo_of_shape()
   results = get_sightings_by_shape(shape)
   return display_results(results)
 
