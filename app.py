9ecf2e87fa0a970960998b03a332c960be1aa79c
root <root@LAPTOP-T1J3CC8U.>
2025-05-12 21:48:09 -0500

    trying again

--- app.py
+++ app.py
@@ -3,24 +3,21 @@ from ProductionCode.processor import (
     filter_by_shape,
     get_sightings_by_shape
 )
-import cl
+
 from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def home_screen()
+def home_screen():
     print("type in shape:/<the shape of the ufos you want to look up> to look up those ufos")
 
-
+'''
 @app.route('shape:/<shape>')
-def display_ufo_of_shape()
+def display_ufo_of_shape():
   results = get_sightings_by_shape(shape)
   return display_results(results)
-
-
-
-
+'''
 
 
 if __name__ == '__main__':
-  app.run(host='0.0.0.0', port=81)
+  app.run()
