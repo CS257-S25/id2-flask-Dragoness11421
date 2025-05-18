130491bf2422917f169d68b468e9ef46bde9966e
root <root@LAPTOP-T1J3CC8U.>
2025-05-18 15:35:13 -0500

    removed unecessary imports and fixed routes

--- app.py
+++ app.py
@@ -1,22 +1,24 @@
+'''
+Flask app for individual deliverable two that returns ufos of a certain shape on command
+'''
 from ProductionCode.processor import (
     display_results,
-    filter_by_shape,
     get_sightings_by_shape
 )
-
 from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
 def home_screen():
-    print("type in shape:/<the shape of the ufos you want to look up> to look up those ufos")
+    """The homescreen of the application. Tells the user viable """
+    return("type in shape/<the shape of the ufos you want to look up> to look up those ufos")
 
-'''
-@app.route('shape:/<shape>')
-def display_ufo_of_shape():
+
+@app.route('shape/<shape>')
+def display_ufo_of_shape(shape):
+  """Returns a ufo of a specified shape"""
   results = get_sightings_by_shape(shape)
   return display_results(results)
-'''
 
 
 if __name__ == '__main__':
