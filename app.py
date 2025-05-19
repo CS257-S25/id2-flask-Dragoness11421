042cd2107b27dfa7f9833e5dface8cc15bce578c
root <root@LAPTOP-T1J3CC8U.>
2025-05-19 12:02:32 -0500

    cleaned up style

--- app.py
+++ app.py
@@ -1,16 +1,16 @@
 '''
 Flask app for individual deliverable two that returns ufos of a certain shape on command
 '''
+from flask import Flask
 from ProductionCode.processor import (
     display_results,
     get_sightings_by_shape
 )
-from flask import Flask
 app = Flask(__name__)
 @app.route('/')
 def home_screen():
-    """The homescreen of the application. Tells the user viable """
-    return("type in shape/<the shape of the ufos you want to look up> to look up those ufos")
+  """The homescreen of the application. Tells the user viable """
+  return "type in /shape/(the shape of the ufos you want to look up) to look up those ufos"
 @app.route('/shape/<shape>')
 def display_ufo_of_shape(shape):
   """Returns a ufo of a specified shape"""
